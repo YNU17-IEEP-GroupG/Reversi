ffa675d9e72fc292f181ca31051e806096c252d5
HRSNOZOMU <hirose-nozomu-jr@ynu.jp>
2017-05-16 15:29:27 +0900

    設定画面もう少し実装.ユーザ情報変更ダイアログ

--- src/jp/ac/ynu/pl2017/gg/reversi/gui/SettingsPanel.java
+++ src/jp/ac/ynu/pl2017/gg/reversi/gui/SettingsPanel.java
@@ -1,16 +1,22 @@
 package jp.ac.ynu.pl2017.gg.reversi.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dialog.ModalityType;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.Icon;
+import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class SettingsPanel extends JPanel {
@@ -19,6 +25,13 @@ public class SettingsPanel extends JPanel {
 	 * 
 	 */
 	private static final long	serialVersionUID	= 1395349167588587208L;
+	
+	private String	username = "6s";
+	private int		icon;
+	private int		onlineWL;
+	private int		hardCPUWL[][];
+	private int		mediumCPUWL[][];
+	private int		basicCPUWL[][];
 
 	public SettingsPanel(TitlePanel.Transition callback) {
 		super();
@@ -57,6 +70,7 @@ public class SettingsPanel extends JPanel {
 		
 		// 変更ボタン
 		JButton lInfoChangeButton = new JButton("アカウント情報変更");
+		lInfoChangeButton.addActionListener(e -> showChangeInfoDialog());
 		JButton lBackChangeButton = new JButton("背景変更");
 		
 		lNamePanel.add(lNameLabel, BorderLayout.NORTH);
@@ -118,13 +132,67 @@ public class SettingsPanel extends JPanel {
 		add(returnButton, BorderLayout.SOUTH);
 	}
 
-	@Override
-	public void validate() {
-		super.validate();
+	public void showChangeInfoDialog() {
+		JDialog lChangeDialog = new JDialog();
+		lChangeDialog.setPreferredSize(new Dimension(400, 170));
+		lChangeDialog.setModalityType(ModalityType.APPLICATION_MODAL);
+		lChangeDialog.setResizable(false);
+		lChangeDialog.getContentPane().setLayout(new FlowLayout());
+		
+		JLabel lNameLabel = new JLabel("名前");
+		JLabel lPassLabel1 = new JLabel("パスワード");
+		JLabel lPassLabel2 = new JLabel("パスワード（確認）");
+		
+		JTextField lNameField = new JTextField(username);
+		JPasswordField lPassField1 = new JPasswordField();
+		JPasswordField lPassField2 = new JPasswordField();
+		
+		JPanel lChangePanel = new JPanel();
+		lChangePanel.setPreferredSize(new Dimension(360, lNameField.getPreferredSize().height * 3));
+		
+		GroupLayout layout = new GroupLayout(lChangePanel);
+		lChangePanel.setLayout(layout);
+		
+		GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
+		hGroup.addGroup(layout.createParallelGroup()
+				.addComponent(lNameLabel).addComponent(lNameField));
+		hGroup.addGroup(layout.createParallelGroup()
+				.addComponent(lPassLabel1).addComponent(lPassField1));
+		hGroup.addGroup(layout.createParallelGroup()
+				.addComponent(lPassLabel2).addComponent(lPassField2));
+		layout.setVerticalGroup(hGroup);
+		
+		GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
+		vGroup.addGroup(layout.createParallelGroup()
+				.addComponent(lNameLabel).addComponent(lPassLabel1).addComponent(lPassLabel2));
+		vGroup.addGroup(layout.createParallelGroup()
+				.addComponent(lNameField).addComponent(lPassField1).addComponent(lPassField2));
+		layout.setHorizontalGroup(vGroup);
+		
+		lChangeDialog.getContentPane().add(lChangePanel);
+		
+		JButton lOKButton = new JButton("OK");
+		lOKButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				lChangeDialog.dispose();
+			}
+		});
+		JButton lCancelButton = new JButton("取消");
+		lCancelButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				lChangeDialog.dispose();
+			}
+		});
 
-		if (getParent() != null) {
-			setSize(getParent().getSize());
-		}
+		lChangeDialog.getContentPane().add(lOKButton);
+		lChangeDialog.getContentPane().add(lCancelButton);
+
+		lChangeDialog.pack();
+		lChangeDialog.setVisible(true);
 	}
 
 }
