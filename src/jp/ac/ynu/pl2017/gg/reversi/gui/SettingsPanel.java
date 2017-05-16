35cbd3656745c740d8e5ce805d75af7ed70d3517
HRSNOZOMU <hirose-nozomu-jr@ynu.jp>
2017-05-16 15:49:37 +0900

    アイコンと背景選択ダイアログ

--- src/jp/ac/ynu/pl2017/gg/reversi/gui/SettingsPanel.java
+++ src/jp/ac/ynu/pl2017/gg/reversi/gui/SettingsPanel.java
@@ -1,6 +1,7 @@
 package jp.ac.ynu.pl2017.gg.reversi.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dialog.ModalityType;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -18,6 +19,7 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
 
 public class SettingsPanel extends JPanel {
 
@@ -26,12 +28,14 @@ public class SettingsPanel extends JPanel {
 	 */
 	private static final long	serialVersionUID	= 1395349167588587208L;
 	
-	private String	username = "6s";
-	private int		icon;
+	private String	username	= "6s";
 	private int		onlineWL;
 	private int		hardCPUWL[][];
 	private int		mediumCPUWL[][];
 	private int		basicCPUWL[][];
+	private int		iconIndex	= 0;
+	private int		backIndex	= 0;
+	private int		temp		= 0;
 
 	public SettingsPanel(TitlePanel.Transition callback) {
 		super();
@@ -55,6 +59,7 @@ public class SettingsPanel extends JPanel {
 		lIconLabel.setSize(new Dimension(200, 200));
 		lIconPanel.add(lIconLabel, BorderLayout.CENTER);
 		JButton lIconChanger = new JButton("アイコンの変更");
+		lIconChanger.addActionListener(e -> showImageSelectDialog(0));
 		lIconPanel.add(lIconChanger, BorderLayout.SOUTH);
 
 		lIconNamePanel.add(lIconPanel, BorderLayout.WEST);
@@ -72,6 +77,7 @@ public class SettingsPanel extends JPanel {
 		JButton lInfoChangeButton = new JButton("アカウント情報変更");
 		lInfoChangeButton.addActionListener(e -> showChangeInfoDialog());
 		JButton lBackChangeButton = new JButton("背景変更");
+		lBackChangeButton.addActionListener(e -> showImageSelectDialog(1));
 		
 		lNamePanel.add(lNameLabel, BorderLayout.NORTH);
 		lNamePanel.add(lInfoChangeButton);
@@ -134,6 +140,7 @@ public class SettingsPanel extends JPanel {
 
 	public void showChangeInfoDialog() {
 		JDialog lChangeDialog = new JDialog();
+		lChangeDialog.setTitle("ユーザ情報変更");
 		lChangeDialog.setPreferredSize(new Dimension(400, 170));
 		lChangeDialog.setModalityType(ModalityType.APPLICATION_MODAL);
 		lChangeDialog.setResizable(false);
@@ -193,6 +200,74 @@ public class SettingsPanel extends JPanel {
 
 		lChangeDialog.pack();
 		lChangeDialog.setVisible(true);
+	}
+	
+	/**
+	 * @param mode 1なら背景,0ならアイコン
+	 */
+	public void showImageSelectDialog(final int mode) {
+		JDialog lChangeDialog = new JDialog();
+		lChangeDialog.setTitle(mode == 0 ? "アイコン変更" : "背景変更");
+		lChangeDialog.setSize(new Dimension(400, 170));
+		lChangeDialog.setModalityType(ModalityType.APPLICATION_MODAL);
+		lChangeDialog.setResizable(false);
+		lChangeDialog.getContentPane().setLayout(new FlowLayout());
+		
+		temp = mode == 0 ? iconIndex : backIndex;
+		
+		JPanel lImagePanel = new JPanel(new GridLayout(1, 4));
+		lImagePanel.setPreferredSize(new Dimension(360, 90));
+		JButton lSelectButtons[] = new JButton[4];
+		for (int i = 0; i < 4; i++) {
+			final int ii = i;
+			lSelectButtons[i] = new JButton();
+			lSelectButtons[i].setPreferredSize(new Dimension(90, 90));
+			lSelectButtons[i].addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					selectButton(lSelectButtons, ii);
+					temp = ii;
+				}
+			});
+			lImagePanel.add(lSelectButtons[i]);
+		}
+		selectButton(lSelectButtons, temp);
+		lChangeDialog.getContentPane().add(lImagePanel);
+		
+		JButton lOKButton = new JButton("OK");
+		lOKButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if (mode == 0) iconIndex = temp; else backIndex = temp;
+				lChangeDialog.dispose();
+			}
+		});
+		JButton lCancelButton = new JButton("取消");
+		lCancelButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				lChangeDialog.dispose();
+			}
+		});
+		
+		lChangeDialog.getContentPane().add(lOKButton);
+		lChangeDialog.getContentPane().add(lCancelButton);
+
+		lChangeDialog.validate();
+		lChangeDialog.setVisible(true);
+	}
+	
+	private static void selectButton(JButton[] buttons, int i) {
+		for (int j = 0; j < buttons.length; j++) {
+			if (j == i) {
+				buttons[j].setBorder(new LineBorder(Color.BLUE, 2, true));
+			} else {
+				buttons[j].setBorder(new LineBorder(Color.BLACK, 1, false));
+			}
+		}
 	}
 
 }
