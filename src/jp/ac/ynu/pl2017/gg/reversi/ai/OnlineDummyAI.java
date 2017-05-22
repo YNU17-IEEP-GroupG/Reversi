9ece56bb8223cb5a08c55dff87071e5e96e63f00
HRSNOZOMU <hirose-nozomu-jr@ynu.jp>
2017-05-23 03:03:14 +0900

    オンライン対戦中にCPUが動く問題に対処

--- src/jp/ac/ynu/pl2017/gg/reversi/ai/OnlineDummyAI.java
+++ src/jp/ac/ynu/pl2017/gg/reversi/ai/OnlineDummyAI.java
@@ -10,23 +10,22 @@ public class OnlineDummyAI extends BaseAI {
 
 	public OnlineDummyAI(List<Point> hint, Stone stone, Stone[][] board, int difficulty) {
 		super(hint, stone, board, difficulty);
+		row = -1;
+		column = -1;
 	}
 
 	@Override
 	public void think() {
-		new Thread() {
-			@Override
-			public void run() {
-				int pos[] = ClientConnection.receivePutStone();
-				if (pos == null) {
-					row = -1;
-					column = -1;
-				}
-				row = pos[0];
-				column = pos[1];
-			}
-		}.start();
+		// 教訓：下手に別スレッドで待ってはいけない
+		int pos[] = ClientConnection.receivePutStone();
+		if (pos == null) {
+			row = -1;
+			column = -1;
+		}
+		row = pos[0];
+		column = pos[1];
 	}
+	
 	@Override
 	public void randomThink() {
 	}
