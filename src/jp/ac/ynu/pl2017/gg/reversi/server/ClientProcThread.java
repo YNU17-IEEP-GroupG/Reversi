100980ddf0fe8b98eb39f23776c0be570896b858
mukuhope <mukuhope@outlook.jp>
2017-05-22 15:41:24 +0900

    通信クラスの訂正

--- src/jp/ac/ynu/pl2017/gg/reversi/server/ClientProcThread.java
+++ src/jp/ac/ynu/pl2017/gg/reversi/server/ClientProcThread.java
@@ -1,7 +1,14 @@
 package jp.ac.ynu.pl2017.gg.reversi.server;
 
+import jp.ac.ynu.pl2017.gg.reversi.util.User;
+
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -11,85 +18,87 @@ class ClientProcThread extends Thread {
 	private int number;// 自分の番号
 	private Socket incoming;// 自分のソケット
 	private Socket Eincoming; // 対戦相手のソケット
-	private InputStreamReader myIsr;
+	// private InputStreamReader myIsr;
+	private InputStream myIs;
+	private OutputStream myOs;
 	private BufferedReader myIn;
 	private PrintWriter myOut;
+
 	private String myName;// 接続者の名前
 	private String pass;// 接続者のパスワード
 	private boolean battle = false;// 対局可能かどうか
 	private boolean turn = false;// true:先手/false:後手
 	private int myRoom = 0;
 
-	public static final int MAX_ROOM = 10;
+	public static final int MAX_ROOM = 20;
 	static int room = 0; // 対局が行われている数
 	static boolean[] change = new boolean[MAX_ROOM];
 	static String[][] coordinate = new String[MAX_ROOM][2];// 通信路
+	static Object item[] = new Object[MAX_ROOM];// アイテムオブジェクトを格納
+	static String itemName[] = new String[MAX_ROOM];
 	static int[] rematch = new int[MAX_ROOM];// 再戦用0:default,1:再戦,2:拒否,3:再戦受付
 
 	static HashMap<String, Match> MatchMap = new HashMap<String, Match>(); // ユーザ名をキーにする
-	static ArrayList<String> randomList = new ArrayList<String>(); 
-	
+	static ArrayList<String> randomList = new ArrayList<String>();
+
 	public static final String PASS = "pass";// テスト用
 
-	public ClientProcThread(int n, Socket i, InputStreamReader isr,
-			BufferedReader in, PrintWriter out) {
+	public ClientProcThread(int n, Socket i, InputStream is, OutputStream os) {
 		number = n;
 		incoming = i;
-		myIsr = isr;
-		myIn = in;
-		myOut = out;
+		myIs = is;
+		myOs = os;
+		myIn = new BufferedReader(new InputStreamReader(is));
+		myOut = new PrintWriter(os, true);
+		// myOis = new ObjectInputStream(is);
+		// myOos = new ObjectOutputStream(os);
 	}
 
 	public void run() {
 		try {
 			/*
-			myOut.println("Hello, client No."
-					+ number
-					+ "¥nコマンド一覧\n"
-					+ "*******************************\n"
-					+ "END:終了　MATCH:マッチング　CANSEL:キャンセル　WRITE:座標送信　READ:座標読み込み　TURN:ターン確認　r:更新\n"
-					+ "*******************************");// 初回だけ呼ばれる
-			*/
-			
-			String firstCmd = myIn.readLine();
-			
-			if(firstCmd.equals("LOGIN")){
-				myName = myIn.readLine();
-				pass = myIn.readLine();
-			}else if(firstCmd.equals("CREATE")){
-				String newMyName = myIn.readLine();
-				String newPass = myIn.readLine();
-				if(Access.makeNewUser(newMyName, newPass)){
-					System.out.println("アカウント作成:"+newMyName);
-					myOut.println("TRUE");
-					myName = newMyName;
-					pass = newPass;
-				}else{
-					myOut.println("FALSE");
-				}
-			}
-			
+			 * myOut.println("Hello, client No." + number + "¥nコマンド一覧\n" +
+			 * "*******************************\n" +
+			 * "END:終了　MATCH:マッチング　CANSEL:キャンセル　WRITE:座標送信　READ:座標読み込み　TURN:ターン確認　r:更新\n"
+			 * + "*******************************");// 初回だけ呼ばれる
+			 */
+
+			/*
+			 * String firstCmd = myIn.readLine();
+			 * 
+			 * if(firstCmd.equals("LOGIN")){ myName = myIn.readLine(); pass =
+			 * myIn.readLine(); }else if(firstCmd.equals("CREATE")){ String
+			 * newMyName = myIn.readLine(); String newPass = myIn.readLine();
+			 * if(Access.makeNewUser(newMyName, newPass)){
+			 * System.out.println("アカウント作成:"+newMyName); myOut.println("TRUE");
+			 * myName = newMyName; pass = newPass; }else{
+			 * myOut.println("FALSE"); } }
+			 */
+
+			myName = myIn.readLine();
+			pass = myIn.readLine();
+
 			if (Access.login(myName, pass)) {
 				System.out.println(myName + ": ログイン完了");
-				myOut.println("TRUE");//ログイン成功のコマンド送信
+				myOut.println("TRUE");// ログイン成功のコマンド送信
 
 				while (true) {// 無限ループで，ソケットへの入力を監視する
 					String cmd = myIn.readLine();
-					
+
 					if (cmd != null) {// このソケット（バッファ）に入力があるかをチェック
-						
-						System.out.println("Received from client No." + number+ "(" + myName + "), Messages: " + cmd);
-						
+
+						System.out.println("Received from client No." + number
+								+ "(" + myName + "), Messages: " + cmd);
+
 						if (cmd.equals("END")) {
-						//	myOut.println("終了します");
+							// myOut.println("終了します");
 							break;
 						}
-					
 
 						if (cmd.equals("MATCH")) {
 							System.out.println(myName + ": マッチング準備");
 
-						//	myOut.println("対戦相手の名前を入力して下さい");
+							// myOut.println("対戦相手の名前を入力して下さい");
 							String enemyName = myIn.readLine();
 							if (!enemyName.equals("CANSEL")
 									&& !enemyName.equals(myName)) {// 自分の名前をいれたらキャンセルされる
@@ -100,17 +109,19 @@ class ClientProcThread extends Thread {
 								MatchMap.put(myName, myMatch);
 								System.out.println(myName + ": " + enemyName
 										+ "に対戦申し込み");
-							//	myOut.println(enemyName
-							//			+ " に対戦を申し込みしました。しばらくお待ちください");
+								// myOut.println(enemyName
+								// + " に対戦を申し込みしました。しばらくお待ちください");
 
 								while (!battle) {// マッチング待ち
 
 									if (MatchMap.containsKey(enemyName)) {// 対戦相手がMatchMapに登録されているかチェック
 
-										if (((MatchMap.get(enemyName)).getEnemyName()).equals(myName)) { // 対戦相手が自分を指名しているかチェック
-											myRoom = (MatchMap.get(enemyName)).getRoom();// 対戦相手のルーム番号を取得
-										//	myOut.printf(enemyName
-										//			+ " とマッチングしました!　　あなたは");
+										if (((MatchMap.get(enemyName))
+												.getEnemyName()).equals(myName)) { // 対戦相手が自分を指名しているかチェック
+											myRoom = (MatchMap.get(enemyName))
+													.getRoom();// 対戦相手のルーム番号を取得
+											// myOut.printf(enemyName
+											// + " とマッチングしました!　　あなたは");
 
 											battle = true;
 
@@ -122,7 +133,7 @@ class ClientProcThread extends Thread {
 									} else {// 先に接続しているので先手にする
 										turn = true;
 									}
-									
+
 									String matching = myIn.readLine();
 
 									if (matching != null) { // マッチングキャンセル
@@ -130,7 +141,7 @@ class ClientProcThread extends Thread {
 											MatchMap.remove(myName);
 											System.out.println(myName
 													+ ": マッチングキャンセル");
-										//	myOut.println("キャンセルしました");
+											// myOut.println("キャンセルしました");
 											break;
 										}
 									}
@@ -144,78 +155,81 @@ class ClientProcThread extends Thread {
 
 							} else {
 								System.out.println(myName + ": マッチングキャンセル");
-							//	myOut.println("キャンセルしました");
+								// myOut.println("キャンセルしました");
 								myOut.println("FALSE");
 							}
-							
+
 						}
-						
-						if(cmd.equals("RANDOM")){
+
+						if (cmd.equals("RANDOM")) {
 							System.out.println(myName + ": ランダムマッチング");
-							String enemyName=null;
-							
-							if(randomList.size()==0){
+							String enemyName = null;
+
+							if (randomList.size() == 0) {
 								randomList.add(myName);
 								turn = true;
-								while(true){
+								while (true) {
 									String matching = myIn.readLine();
-									
-									if(randomList.size()==2){
+
+									if (randomList.size() == 2) {
 										enemyName = randomList.get(1);
 										randomList.remove(0);
 										randomList.remove(0);
-										
+
 										myRoom = room;
 										room++;// room番号をインクリメント
 										break;
 									}
-									
+
 									if (matching != null) { // マッチングキャンセル
 										if (matching.equals("CANSEL")) {
 											randomList.remove(0);
-											System.out.println(myName+ ": マッチングキャンセル");
-										//	myOut.println("キャンセルしました");
+											System.out.println(myName
+													+ ": マッチングキャンセル");
+											// myOut.println("キャンセルしました");
 											break;
 										}
 									}
 								}
-								
-							}else{
+
+							} else {
 								myRoom = room;
 								enemyName = randomList.get(0);
 								randomList.add(myName);
 							}
-							
+
 							battle = true;
 							change[myRoom] = true;
 							rematch[myRoom] = 0;
 							System.out.println(myName + ": マッチング完了");
 							myOut.println(enemyName);
-							
-							
+
 						}
-						
+
 						if (cmd.equals("WRITE")) {
 							if (battle) {
 								System.out.println(myName + ": 座標の更新");
-								
+
 								if (change[myRoom] == turn) {
-								//	myOut.println("座標を入力して下さい");
+									// myOut.println("座標を入力して下さい");
 									coordinate[myRoom][0] = myIn.readLine();
 									coordinate[myRoom][1] = myIn.readLine();
-									
-								//	myOut.println(coordinate[myRoom]+ " におきました");
+
+									// myOut.println(coordinate[myRoom]+
+									// " におきました");
 									myOut.println("TRUE");
-									
-									System.out.println(myName+" が "+coordinate[myRoom][0]+","+coordinate[myRoom][1]+"におきました");
-									
+
+									System.out.println(myName + " が "
+											+ coordinate[myRoom][0] + ","
+											+ coordinate[myRoom][1] + "におきました");
+
 									change[myRoom] = !change[myRoom];// ターンを切り替える
 								} else {
-								//	myOut.println("相手のターンです");
+									// myOut.println("相手のターンです");
 									myOut.println("FALSE");
 								}
 							} else {
-							//	myOut.println("対局が始まってません");
+								// myOut.println("対局が始まってません");
 								myOut.println("FALSE");
 							}
 
@@ -229,61 +243,81 @@ class ClientProcThread extends Thread {
 
 						if (cmd.equals("TURN")) {// 自分のターンかどうかを確かめる
 							if (change[myRoom] == turn) {
-							//	myOut.println("あなたのターンです");
+								// myOut.println("あなたのターンです");
 								myOut.println("TRUE");
 							} else {
-							//	myOut.println("相手のターンです");
+								// myOut.println("相手のターンです");
 								myOut.println("FALSE");
 							}
 						}
 
+						if (cmd.equals("ITEM_SEND")) {// アイテム送信
+							ObjectInputStream myOis = new ObjectInputStream(
+									myIs);
+							itemName[myRoom] = myIn.readLine();// アイテム名を受信
+							item[myRoom] = myOis.readObject();// アイテムオブジェクトを受信
+							if (item[myRoom] != null) {
+								myOut.println("TRUE");
+							} else {
+								myOut.println("FALSE");
+							}
+						}
+
+						if (cmd.equals("ITEM_RECIVE")) {// アイテム受信
+							ObjectOutputStream myOos = new ObjectOutputStream(
+									myOs);
+							myOos.writeObject(item[myRoom]);// クライアントへ送信
+							item[myRoom] = null;// オブジェクトを初期化
+						}
+
 						if (cmd.equals("REMATCH")) {
 							if (rematch[myRoom] == 0) {// 相手が未応答
-							//	myOut.println("再戦:1,終了:2");
-								rematch[myRoom] = Integer.parseInt(myIn.readLine());
+								// myOut.println("再戦:1,終了:2");
+								rematch[myRoom] = Integer.parseInt(myIn
+										.readLine());
 
 								if (rematch[myRoom] == 1) {
-								//	myOut.println("対戦相手を待っています");
+									// myOut.println("対戦相手を待っています");
 									while (true) {// 相手待ち
-										
-										
+
 										String cansel = myIn.readLine();
 										if (cansel.equals("CANSEL")) {
 											myOut.println("キャンセルしました");
 											rematch[myRoom] = 2;
 											break;
 										}
-										
-										
+
 										if (rematch[myRoom] == 2) {
-										//	myOut.println("再戦が拒否されました");
+											// myOut.println("再戦が拒否されました");
 											myOut.println("FALSE");
 											break;
 										} else if (rematch[myRoom] == 3) {
-										//	myOut.println("再戦を行います");
+											// myOut.println("再戦を行います");
 											myOut.println("TRUE");
 											break;
 										}
 									}
 								} else if (rematch[myRoom] == 2) {
-								//	myOut.println("終了します");
+									// myOut.println("終了します");
 									myOut.println("FALSE");
 								} else {
-								//	myOut.println("不正な値です");
+									// myOut.println("不正な値です");
 									rematch[myRoom] = 0;
 								}
 							} else {// 相手が応答済
 								if (rematch[myRoom] == 2) {
-								//	myOut.println("再戦が拒否されました");
-									rematch[myRoom] = Integer.parseInt(myIn.readLine());
+									// myOut.println("再戦が拒否されました");
+									rematch[myRoom] = Integer.parseInt(myIn
+											.readLine());
 									rematch[myRoom] = 0;
 									myOut.println("FALSE");
-									
+
 								} else if (rematch[myRoom] == 1) {
-								//	myOut.println("再戦:1,終了:2");
-									rematch[myRoom] = Integer.parseInt(myIn.readLine());
+									// myOut.println("再戦:1,終了:2");
+									rematch[myRoom] = Integer.parseInt(myIn
+											.readLine());
 									if (rematch[myRoom] == 1) {
-								//		myOut.println("再戦を行います");
+										// myOut.println("再戦を行います");
 										myOut.println("TRUE");
 										rematch[myRoom] = 3;
 									} else if (rematch[myRoom] == 2) {
@@ -295,22 +329,31 @@ class ClientProcThread extends Thread {
 								}
 							}
 						}
-						
+
+						if (cmd.equals("USER")) {
+							User user;
+							ObjectOutputStream myOos = new ObjectOutputStream(
+									myOs);
+
+							System.out.println(myName + ": ユーザ情報読み込み");
+							user = Access.requestFullUserData(myName);
+							myOos.writeObject(user);
+						}
+
 						if (cmd.equals("UPDATE")) {
 							String newName = myIn.readLine();
 							String newPass = myIn.readLine();
-							if(Access.updateUser(myName, newName, newPass)){
+							if (Access.updateUser(myName, newName, newPass)) {
 								myOut.println("TRUE");
-								System.out.println(myName+" >> "+newName+"   "+pass+" >> "+"newPass");
+								System.out.println(myName + " >> " + newName
+										+ "   " + pass + " >> " + "newPass");
 								myName = newName;
 								pass = newPass;
-							}else{
+							} else {
 								myOut.println("FALSE");
-							}	
+							}
 						}
-						
-						
-						
+
 						// MyServer.SendAll(str, myName);//
 						// サーバに来たメッセージは接続しているクライアント全員に配る
 					}
@@ -325,10 +368,9 @@ class ClientProcThread extends Thread {
 			System.out.println("Disconnect from client No." + number + "("
 					+ myName + ")");
 			Server.SetFlag(number, false);// 接続が切れたのでフラグを下げる
-		} finally{
+		} finally {
 			Access.closeConnection();
-			System.out.println(myName+" DataBaseのclose");
+			System.out.println(myName + " DataBaseのclose");
 		}
 	}
 }
-
--- src/jp/ac/ynu/pl2017/gg/reversi/server/Server.java
+++ src/jp/ac/ynu/pl2017/gg/reversi/server/Server.java
@@ -1,23 +1,34 @@
 package jp.ac.ynu.pl2017.gg.reversi.server;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.Serializable;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-class Server {
+class Server implements Serializable{
 
     private static int maxConnection = 100;// 最大接続数
     private static Socket[] incoming;// 受付用のソケット
     private static boolean[] flag;// 接続中かどうかのフラグ
-    private static InputStreamReader[] isr;// 入力ストリーム用の配列
-    private static BufferedReader[] in;// バッファリングをによりテキスト読み込み用の配列
-    private static PrintWriter[] out;// 出力ストリーム用の配列
+    //private static InputStreamReader[] isr;// 入力ストリーム用の配列
+    //private static BufferedReader[] in;// バッファリングをによりテキスト読み込み用の配列
+    //private static PrintWriter[] out;// 出力ストリーム用の配列
+    //private static ObjectInputStream[] ois;// オブジェクト受け渡し用の配列
+    //private static ObjectOutputStream[] oos;
+    
+    private static InputStream[] is;
+    private static OutputStream[] os;
+    
     private static ClientProcThread[] myClientProcThread;// スレッド用の配列
     private static int member;// 接続しているメンバーの数
 
-    // 全員にメッセージを送る
+    /*
     public static void SendAll(String str, String myName) {
         // 送られた来たメッセージを接続している全員に配る
         for (int i = 1; i <= member; i++) {
@@ -28,6 +39,7 @@ class Server {
             }
         }
     }
+    */
 
     // フラグの設定を行う
     public static void SetFlag(int n, boolean value) {
@@ -39,9 +51,15 @@ class Server {
         // 必要な配列を確保する
         incoming = new Socket[maxConnection];
         flag = new boolean[maxConnection];
-        isr = new InputStreamReader[maxConnection];
-        in = new BufferedReader[maxConnection];
-        out = new PrintWriter[maxConnection];
+        //isr = new InputStreamReader[maxConnection];
+        //in = new BufferedReader[maxConnection];
+        //out = new PrintWriter[maxConnection];
+        //ois = new ObjectInputStream[maxConnection];
+        //oos = new ObjectOutputStream[maxConnection];
+        
+        is = new InputStream[maxConnection];
+        os = new OutputStream[maxConnection];
+        
         myClientProcThread = new ClientProcThread[maxConnection];
 
         int n = 1;
@@ -49,24 +67,34 @@ class Server {
 
         try {
             System.out.println("サーバが起動しました");
-            ServerSocket server = new ServerSocket(10000);// 10000番ポートを利用する
+            ServerSocket server = new ServerSocket(50000);// 50000番ポートを利用する
+            
             while (true) {
                 incoming[n] = server.accept();
                 flag[n] = true;
                 System.out.println("Accept client No." + n);
                 // 必要な入出力ストリームを作成する
-                isr[n] = new InputStreamReader(incoming[n].getInputStream());
+                //isr[n] = new InputStreamReader(incoming[n].getInputStream());
+                
+                is[n] = incoming[n].getInputStream();
+                os[n] = incoming[n].getOutputStream();
+                
+                /*
                 in[n] = new BufferedReader(isr[n]);
                 out[n] = new PrintWriter(incoming[n].getOutputStream(), true);
-
-                myClientProcThread[n] = new ClientProcThread(n, incoming[n],
-                        isr[n], in[n], out[n]);// 必要なパラメータを渡しスレッドを作成
+                
+                ois[n] = new ObjectInputStream(incoming[n].getInputStream());
+				oos[n] = new ObjectOutputStream(incoming[n].getOutputStream());
+				*/
+                
+                myClientProcThread[n] = new ClientProcThread(n, incoming[n], is[n], os[n]);
                 myClientProcThread[n].start();// スレッドを開始する
                 member = n;// メンバーの数を更新する
                 n++;
             }
         } catch (Exception e) {
             System.err.println("ソケット作成時にエラーが発生しました: " + e);
+            e.printStackTrace();
         }
     }
 }
